67e5d86b1c1c7d961afcb799b6df9879b1c49f49
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-12-08 13:57:02 +0200

    added routing and adjusting it

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+};
+
+export default AppLayout;
