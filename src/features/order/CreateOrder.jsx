8634e11875a07f79c2893f5c5acd23e71253e892
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-12-15 10:24:18 +0200

    added button styling, links and adjusted them

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -4,7 +4,7 @@ import { createOrder } from "../../services/apiRestaurant";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -39,13 +39,15 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <h2>Ready to order? Let's go!</h2>
 
-      <Form method="POST">
+      <Form method="POST" className="py-4">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <div>
+            <input type="text" name="customer" required />
+          </div>
         </div>
 
         <div>
@@ -76,7 +78,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="my-3 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-500 disabled:text-stone-100"
+          >
             {isSubmitting ? "Placing Order.." : "Order now"}
           </button>
         </div>
@@ -88,18 +93,19 @@ function CreateOrder() {
 export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  const order = {
-    ...data,
-    cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
-  };
+  // const order = {
+  //   ...data,
+  //   cart: JSON.parse(data.cart),
+  //   priority: data.priority === "on",
+  // };
 
-  const errors = {};
-  if (!isValidPhone(order.phone))
-    errors.phone = "Please use correct phone number";
-  if (Object.keys(errors).length > 0) return errors;
-  const newOrder = await createOrder(order);
-  return redirect(`/order/${newOrder.id}`);
+  // const errors = {};
+  // if (!isValidPhone(order.phone))
+  // errors.phone = "Please use correct phone number";
+  // if (Object.keys(errors).length > 0) return errors;
+  // const newOrder = await createOrder(order);
+  // return redirect(`/order/${newOrder.id}`);
+  return null;
 }
 
 export default CreateOrder;
