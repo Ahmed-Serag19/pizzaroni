80adc707f5f14001643f6c212f19e4e40c3d8ebf
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-12-16 13:12:49 +0200

    added button variations

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,16 @@
-function Button({ children, disabled }) {
+import { Link } from "react-router-dom";
+
+function Button({ children, disabled, to }) {
+  const className =
+    "my-3 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-500 disabled:text-stone-100 sm:px-6 sm:py-4";
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
   return (
-    <button
-      disabled={disabled}
-      className="my-3 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-500 disabled:text-stone-100"
-    >
+    <button disabled={disabled} className={className}>
       {children}
     </button>
   );
