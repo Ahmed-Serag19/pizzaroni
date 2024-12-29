6326ae49310c64c413a33e2253929a7ce58a474f
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-12-29 13:44:19 +0200

    adjusted cart and created slice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: quantity * unitPrice,
+    },
+  ],
+};
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      state.cart.push(action.payload);
+    },
+    deletefromCart(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+export const {
+  addToCart,
+  deletefromCart,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { updateName } from "./userSlice";
 import { useNavigate } from "react-router-dom";
 
@@ -8,6 +8,7 @@ function CreateUser() {
   const [username, setUsername] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const currentUser = useSelector((state) => state.user.username);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -18,17 +19,30 @@ function CreateUser() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p className="mb-4 text-sm text-stone-600 md:text-base">
-        👋 Welcome! Please start by telling us your name:
-      </p>
+      {currentUser ? (
+        <div>
+          <p className="mb-4 text-sm text-stone-600 md:text-base">
+            👋 Welcome {currentUser}, Continue ordering!
+          </p>
+          <Button to="/menu" type="small">
+            Menu
+          </Button>
+        </div>
+      ) : (
+        <p className="mb-4 text-sm text-stone-600 md:text-base">
+          👋 Welcome! Please start by telling us your name:
+        </p>
+      )}
 
-      <input
-        type="text"
-        placeholder="Your full name"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        className="input mb-8 w-72"
-      />
+      {!currentUser && (
+        <input
+          type="text"
+          placeholder="Your full name"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="input mb-8 w-72"
+        />
+      )}
 
       {username !== "" && (
         <div>
